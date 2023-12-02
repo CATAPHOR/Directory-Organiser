da0461f4c8fed19413cf38b8523f746c2ed37621
Adam Gafar <adamelhadi@hotmail.co.uk>
2023-12-02 22:33:29 +0000

    Optional folder moving, error handling, enhanced console output
    - Added user confirmation prompt on whether to organise folders too, or just files
    - Added error handling for when files cannot be moved (e.g. file currently open)
    - Improved console output, showing files that have been moved, as well as their counts and any that could not be moved.

--- dir_organiser.py
+++ dir_organiser.py
@@ -26,16 +26,37 @@ def make_dirs(path, names):
         (path / name).mkdir(exist_ok = True)
 
 # move all files within directory to correct categories
-def move_files(filetype_dict, path, category_folders):
+def move_files(filetype_dict, path, category_folders, ignore_dirs):
+    file_count = 0
+    dir_count = 0
+    errors = []
+    
     for child_path in path.iterdir():
+        # file moving
         if child_path.is_file():
+            # determine directory to move to
             try:
                 new_child_path = path / filetype_dict[child_path.suffix[1:]]
             except KeyError:
                 new_child_path = path / "other"
-            shutil.move(child_path, new_child_path)
-        elif child_path.is_dir():
-            move_folder(filetype_dict, child_path, category_folders)
+            # attempt move operation
+            try:
+                shutil.move(child_path, new_child_path)
+                file_count += 1
+                print(f"FILE:\t'{child_path.name}' -> {new_child_path.name}")
+            except shutil.Error:
+                errors.append(child_path)
+            
+        # folder moving
+        elif child_path.is_dir() and not ignore_dirs:
+            dir_move_output = move_folder(filetype_dict, child_path, category_folders)
+            # update counts if not category folder
+            if dir_move_output[0]:
+                dir_count += dir_move_output[1]
+                if not dir_move_output[1]:
+                    errors.append(child_path)
+            
+    return file_count, dir_count, errors
 
 # determine categories to place folders into; detects frequency of filetypes within
 def move_folder(filetype_dict, path, category_folders):
@@ -59,7 +80,17 @@ def move_folder(filetype_dict, path, category_folders):
             new_path = path.parent / category[0]
         
         # move folder
-        shutil.move(path, new_path)
+        try:
+            shutil.move(path, new_path)
+            print(f"FOLDER:\t'{path.name}' -> .\\{new_path.name}")
+            # bools: [not a category folder]: true; [operation successful]: true
+            return True, True
+        except shutil.Error:
+            # bools: see above
+            return True, False
+    
+    # first bool false as folder was category dir
+    return False, True
 
 # recursively counts number of files of each category in directory and sub-directories
 def count_filetypes(counter, path, filetype_dict):
@@ -93,7 +124,27 @@ if __name__ == "__main__":
         category_folders = list(set(type_dict.values()))
         # make category subfolders
         make_dirs(folder, category_folders)
-        # move files to appropriate categories
-        move_files(type_dict, folder, category_folders)
         
-        print("Folder has been reorganised categorically by file-type.")
+        # get user confirmation on whether to move folders within directory
+        print()
+        while True:
+            try:
+                ignore_dirs = not choice[input("Should the program move folders in "
+                                               "this directory? Y/N: ").lower()]
+            except KeyError:
+                continue
+            break
+        
+        # move files to appropriate categories
+        print("\nMoving files:")
+        file_count, dir_count, errors = move_files(type_dict, folder, category_folders, ignore_dirs)
+        
+        print("\nFolder has been reorganised categorically by file-type.")
+        print(f"{file_count} file{'s' * (file_count != 1)} moved; "
+              f"{dir_count} folder{'s' * (dir_count != 1)} moved.")
+        if len(errors) > 0:
+            print(f"\n{len(errors)} files and/or folders could not be moved:")
+            for path in errors:
+                print(f"\t{path.name}")
+        
+        input("\nPress [Enter] to exit.")
